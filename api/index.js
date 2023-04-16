a581bd65cf11aa3b842141e68be808d0aab63e42
Aimee Ramirez <alramirez1@alumni.fullsail.edu>
2023-04-15 23:35:13 -0400

    api call poc

--- api/index.js
+++ api/index.js
@@ -17,27 +17,31 @@ app.set("port", process.env.PORT || 3000);
 let prompt = "In a NABRE bible religious belief, is God real? Provide a verse from NABRE bible."
 
 //function to start API
-const startAPI = async ()=> {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt,
-    temperature: 0,
-    max_tokens: 1000
-
-  });
-
-console.log(response.data.choices[0].text);
-return response
-};
 
 // defines the first routess
 app.get("/", function (req, res) {
   let intro = `<h1>cos550-aimeelramirez-api</h1>
   <p>This is an open endpoint with not authenication needed at the moment.</p>`;
-  res.send(intro);
-  //returns the request from async await function
-  let apiTest = startAPI();
-  console.log(apiTest);
+    //returns the request from async await function
+    const startAPI = async ()=> {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0,
+        max_tokens: 1000
+    
+      });
+//console.log(response.data.choices[0].text)
+    return response.data.choices[0].text
+    };
+  startAPI().then((data) =>{
+      console.log(data)
+      res.send( intro + "<br/>" + data);
+    })
+    
+
+ 
+
 })
 app.listen(app.get("port"), function () {
   console.log("Node app is running at localhost:" + app.get("port"));
