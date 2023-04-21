e19716a8a7ee188a4b158b9dcd13eafdf5f14d42
Aimee Ramirez <alramirez1@alumni.fullsail.edu>
2023-04-21 19:27:43 -0400

    Commit changes to ui and interface no stylesheets.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Loader from "./Loader/Loader";
 import "./App.css";
 
 function App() {
   const [loaded, setLoaded] = useState<any>(false);
+  const [post, setPost] = useState<any>();
+  const inputRef = useRef<any>(null);
+
+  function handleClick(e:any) {
+    e.preventDefault();
+    console.log(e.target[0].value)
+    setPost(e.target[0].value)
+  }
+
   //create a loader
   if (loaded === false) {
     setTimeout(() => {
@@ -25,19 +34,18 @@ function App() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          > */}
-           Under Construction
-          
+         <p> PraiseAI</p>
+         <p>{post}</p>
+         <div className="content">
+         <hr/>
+         </div>
+          <form ref={inputRef}  onSubmit={handleClick} >
+          <textarea>Write something here</textarea>
+          <hr/>
+          <button title={'Submit'} type="submit">Submit</button>
+          </form>
         </header>
+      <footer>Hi Mommy Milagros</footer>
       </div>
     );
   }
